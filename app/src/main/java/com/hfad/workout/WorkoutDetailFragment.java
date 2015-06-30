abf1967d7610ab337eaa7d9019348d47e3b53fb3
Frank Bertuca, Jr <gloworm@layer01.com>
2015-06-30 16:11:16 -0400

    Added list fragment code

--- app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -1,11 +1,12 @@
 package com.hfad.workout;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -27,6 +28,23 @@ public class WorkoutDetailFragment extends Fragment
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        View view = getView();
+
+        if(view != null)
+        {
+            TextView title = (TextView)view.findViewById(R.id.textTitle);
+            TextView description = (TextView)view.findViewById(R.id.textDescription);
+            Workout workout = Workout.workouts[(int)workoutId];
+
+            title.setText(workout.getName());
+            description.setText(workout.getDescription());
+        }
     }
 
     public void setWorkout(long id)
